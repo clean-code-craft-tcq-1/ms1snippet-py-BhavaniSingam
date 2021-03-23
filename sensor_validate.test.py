4f9203dcb0ec18d6d2e8897df87b0b67c1fa7279
BhavaniSingam <39860323+BhavaniSingam@users.noreply.github.com>
2021-03-23 13:49:08 +0530

    Update sensor_validate.test.py

--- sensor_validate.test.py
+++ sensor_validate.test.py
@@ -5,8 +5,7 @@ class SensorValidatorTest(unittest.TestCase):
   def test_reports_error_when_soccurr_jumps(self):
     self.assertFalse(
       sensor_validate.validate_soccurr_reading([0.0, 0.01, 0.5, 0.51],'soc'),
-      sensor_validate.validate_soccurr_reading([0.03, 0.03, 0.03, 0.33],'curr'),
-      sensor_validate.validate_soccurr_reading(None,'None') 
+      sensor_validate.validate_soccurr_reading([0.03, 0.03, 0.03, 0.33],'curr')      
     )   
 if __name__ == "__main__":
   unittest.main()
